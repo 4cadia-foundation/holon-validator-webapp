8c6af1dfa115115d7101d2b40fdf131684495fd0
Yasmim Vieira <yasmim@janusproj.com>
2019-08-28 17:13:39 -0300

    Create redesign Profile screen and create class at the utils

--- src/modules/Profile/Profile.js
+++ src/modules/Profile/Profile.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Grid, Row, Col,FormControl } from 'react-bootstrap';
+import { Grid, Row, Col, Glyphicon } from 'react-bootstrap';
 
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
@@ -8,9 +8,9 @@ import * as ValidatorActions from '../../redux/actions/validator';
 import CloseIconPage from '../../components/CloseIconPage/CloseIconPage';
 import logo from '../../images/holon38.png';
 import './Profile.css';
+import '../../styles/_utils.css';
 
 class Profile extends Component {
-    // Alterado
     constructor(props) {
         super(props);
         this.state={
@@ -28,37 +28,43 @@ class Profile extends Component {
         return (
             <div>
                 <Grid>
-                     <div className="btn-profile-close">
-                        <CloseIconPage destination="/menu"/>
-                    </div>
-                    <div className="text-center margin-top-30 margin-bottom-30">
-                        <img className="logoHome" src={logo} alt="Logo" />
-                    </div>
                     <Row>
-                        <Col>
-                            &nbsp;
+                        <Col sm={2}>
+                        </Col>
+                        <Col sm={6}>
+                                <div className="title-icon title">
+                                    <Glyphicon glyph="inbox"/>
+                                    <h1>Dashboard</h1>
+                                </div>
+                                <hr className="line-home" />
+                                <h5>Home/Profile</h5>
+                                <h3>Profile</h3>
+                        </Col>
+                        <Col sm={3}>
                         </Col>
                     </Row>
-                    {
-                        this.state.personalInfo.map((val, idx) =>
+                </Grid>
+                {
+                    this.state.personalInfo.map((val, idx) =>
                         {
-                            return(
-                                <Row className="text-center pad10b" key={'row_' + idx.toString()}>
-                                    <Col>
-                                        <FormControl
-                                            id={idx.toString()}
-                                            key={idx.toString()}
-                                            type="text"
-                                            value={val.valor}
-                                            readOnly
-                                            className="text-center"
-                                        />
+                        return(
+                            <Grid>
+                                <Row  className="text-center pad10b" key={'row_' + idx.toString()}>
+                                    <Col sm={2} />
+                                    <Col sm={6}>
+                                        <div className="margin-top-30">
+                                                <div className="card-profile">
+                                                    {idx.toString()}
+                                                    {idx.toString()}
+                                                    {val.valor}
+                                                </div>
+                                        </div>
                                     </Col>
                                 </Row>
+                            </Grid>
                             )
                         })
                     }
-                </Grid>
             </div>
         );
     }
