fbddcffd4915d03be5d1b97bc26175e530779626
Henrique Barcelos <hbarcelos909@gmail.com>
2020-01-04 10:55:08 -0300

    Changed browser history to hash history

--- src/index.js
+++ src/index.js
@@ -8,23 +8,19 @@ import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/main.css';
 
-import {ToastContainer} from "react-toastify";
-import { HashRouter  } from "react-router-dom";
-import { ConnectedRouter } from "connected-react-router";
+import { ToastContainer } from 'react-toastify';
+import { ConnectedRouter } from 'connected-react-router';
 
 ReactDOM.render(
   <Provider store={store}>
-
-    <ToastContainer position={'top-center'} />
+    <ToastContainer position="top-center" />
 
     <ConnectedRouter history={history}>
       <App />
     </ConnectedRouter>
-
   </Provider>,
   document.getElementById('root')
 );
-
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
